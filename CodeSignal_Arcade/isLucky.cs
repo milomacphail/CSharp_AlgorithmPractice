630670751dcea7f7f5ca9185f7a15dea02b4499f
milomacphail <milomacphail@gmail.com>
2019-09-26 20:18:27 -0400

    isLucky algorithm of the day

---
+++ CodeSignal_Arcade/isLucky.cs
@@ -0,0 +1,26 @@
+bool isLuckyNumber(int n) {
+     
+     List <int> nArray = new List <int>();
+     
+    while (n != 0)
+    {
+        nArray.Add(n % 10);
+        n /= 10;
+    }
+     
+     nArray.Reverse();
+    
+     bool isLucky = true;
+     
+     for (int i = 0; i < nArray.Count; i++)
+     {
+          if (nArray[i] != 4 && nArray[i] != 7)
+          {
+               isLucky = false;
+               break;
+          }
+     }
+     
+     return isLucky;
+
+}
