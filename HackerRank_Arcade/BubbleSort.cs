e6072768416abad62cadc68505c5a0f1d2784289
milomacphail <milomacphail@gmail.com>
2019-10-15 22:14:29 -0400

    Bubble Sort

---
+++ HackerRank_Arcade/BubbleSort.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Solution {
+
+    static void countSwaps(int[] a) {
+            
+            for (int p = 0; p <= a.Length - 2; p++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
+                    }
+                } 
+            }
+    }
+}
