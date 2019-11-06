20d7156e61267fa38888dec8aed0d5faad5f2267
milomacphail <milomacphail@gmail.com>
2019-11-06 18:45:20 -0500

    Bubble Sort

--- HackerRank_Arcade/BubbleSort.cs
+++ HackerRank_Arcade/BubbleSort.cs
@@ -1,20 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
 
 class Solution {
 
     static void countSwaps(int[] a) {
-            
-            for (int p = 0; p <= a.Length - 2; p++)
+
+            int swaps = 0;
+
+            for (int j = 0; j <= a.Length - 2; j++)
             {
                 for (int i = 0; i <= a.Length - 2; i++)
                 {
                     if (a[i] > a[i + 1])
                     {
-                        t = a[i + 1];
+                        int temp = a[i + 1];
                         a[i + 1] = a[i];
-                        a[i] = t;
+                        a[i] = temp;
+                        swaps++;
                     }
                 } 
             }
+
+        Console.WriteLine("Array is sorted in {0} swaps.", swaps);
+        Console.WriteLine("First Element: {0}", a[0]);
+        Console.WriteLine("Last Element: {0}", a[a.Length-1]);
+    }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+        countSwaps(a);
     }
 }
