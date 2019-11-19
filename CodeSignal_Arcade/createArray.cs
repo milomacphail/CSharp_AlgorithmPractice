75fad36057566742bd6cfc9b7b3ef8f065382046
milomacphail <milomacphail@gmail.com>
2019-11-18 19:25:40 -0500

    createArray

---
+++ CodeSignal_Arcade/createArray.cs
@@ -0,0 +1,12 @@
+int[] createArray(int size) {
+    
+    int[] arrayOfOnes = new int[size];
+    
+    for(int i = 0; i < arrayOfOnes.Length; i++)
+    {
+        arrayOfOnes[i] = 1;
+    }
+    
+    return arrayOfOnes;
+
+}
