7b616d4505cf8eecb20309cb836c968744fdca15
milomacphail <milomacphail@gmail.com>
2019-11-29 18:42:36 -0500

    Check Palindrome

--- CodeSignal_Challenges/checkPalindrome.cs
+++ CodeSignal_Challenges/checkPalindrome.cs
@@ -1,15 +1,15 @@
 bool checkPalindrome(string inputString) {
 
-    string first = inputString.Substring(0, inputString.Length / 2);
+    string firstHalf = inputString.Substring(0, inputString.Length / 2);
     char[] charArray = inputString.ToCharArray();
     
     Array.Reverse(charArray);
     
     string temp = new string(charArray);
     
-    string second = temp.Substring(0, temp.Length / 2);
+    string secondHalf = temp.Substring(0, temp.Length / 2);
     
-    return first.Equals(second);
+    return firstHalf.Equals(second);
     
     
 }
